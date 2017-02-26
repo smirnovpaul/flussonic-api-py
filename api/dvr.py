dd9f7f1d7a2d4276a781c1ccc62b2f08bece0e9a
Павел Морозкин < >
2017-02-26 20:32:00 +0500

    New method in DVR API

--- api/dvr.py
+++ api/dvr.py
@@ -8,14 +8,18 @@ import requests
 
 class DvrApi(object):
     """
+    Token is a simple method for check.
     http://flussonic.com/doc/dvr/api
     """
-    def __init__(self, url, token, stream_name, from_time, to_time):
-        self.payload = {'from': from_time, 'to': to_time, 'token': token}
-        self.url = 'http://{0}/{1}/recording_status.json'.format(url, stream_name)
+    def __init__(self, url, token, stream_name):
+        self.payload = None
+        self.url = url
+        self.stream_name = stream_name
+        self.token = token
+        self.url = None
 
     @property
-    def get(self):
+    def __connect(self):
         try:
             r = requests.get(self.url, params=self.payload)
         except requests.RequestException as e:
@@ -29,9 +33,70 @@ class DvrApi(object):
             return None
 
         try:
-            r.json()
+            response = r.json()
         except ValueError as e:
             print('Error request json: {}'.format(e))
             return None
         else:
-            return r.json()
+            return response
+
+    def recording_status(self, from_time, to_time):
+        """
+        Recording status.
+
+        ARGUMENTS:
+            :param from_time
+            :param to_time
+            :return: json
+
+        EXAMPLE:
+        [{
+            "stream":"hik2",
+            "ranges":[
+                {"from":1399917362,"duration":65},
+                {"from":1399926545,"duration":102},
+                {"from":1399965549,"duration":350}
+            ],
+            "brief_thumbnails":[
+                1399917362,
+                1399917373,
+                1399917385,
+                1399917397,
+                1399917409,
+                1399917421,
+                1399926545
+            ]
+        }]
+        """
+        self.payload = {'from': from_time, 'to': to_time, 'token': self.token}
+        self.url = 'http://{0}/{1}/recording_status.json'.format(self.url, self.stream_name)
+        return self.__connect()
+
+    @property
+    def lock(self, from_time, duration):
+        """
+        Lock to delete from archive.
+
+        ARGUMENTS:
+            :param from_time
+            :param duration
+            :return: json
+
+        EXAMPLE:
+        [{
+            "stream":"ort",
+            "ranges":[
+                {"duration":3687,"from":1483970675},
+                {"duration":56758,"from":1483974376},
+                {"duration":332,"from":1484031143}],
+            "locks":[
+            {"duration":1004,"from":1483971680}
+            ]
+        }]
+        """
+        self.payload = {
+            'stream': self.stream_name, 'from': from_time,
+            'duration': duration, 'token': self.token
+        }
+        self.url = 'http://{0}/flussonic/api/dvr/lock'.format(self.url)
+        return self.__connect()
